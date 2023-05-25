99b12f8e194dce85869f0832ecfa0f499814899b
Yordi100 <126788047+Yordi100@users.noreply.github.com>
2023-05-25 08:19:49 -0400

    Add files via upload

---
+++ Sketches/sketch1.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  fill(255, 160, 51)
+  ellipse(200,200,120,150)
+  
+  fill(0, 0, 0)
+  ellipse(230,180,30,30)
+  
+  fill(0, 0, 0)
+  ellipse(170,180,30,30)
+  
+   fill(0, 0, 0)
+  line(230,250,100,200)
+  
+}
+
